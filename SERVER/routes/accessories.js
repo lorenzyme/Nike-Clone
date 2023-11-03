aec59bd69337b0f6212c437684148c6b686cb9b8
LookeiCode <137450026+LookeiCode@users.noreply.github.com>
2023-11-02 23:56:33 -0400

    Added all the prisma routes for each table + added a troubleshooting folder

--- SERVER/routes/accessories.js
+++ SERVER/routes/accessories.js
@@ -4,8 +4,9 @@ const router = express.Router();
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
+// ******************************************************************************************
 
-// SEE ALL THE AUTHORS
+// SEE/GET ALL THE ACCESSORIES
 router.get('/', async (req, res) => {
 
     try {
@@ -18,8 +19,11 @@ router.get('/', async (req, res) => {
     }
 })
 
+// POSTMAN "GET" ROUTE --> http://localhost:3000/nike/accessories/
 
+// ******************************************************************************************
 
+// MAKE A NEW ACCESSORY
 router.post('/new', async (req, res) => {
 
     try {
@@ -34,9 +38,70 @@ router.post('/new', async (req, res) => {
         res.json(newAccessories);
     } catch (error) {
         console.log(error);
-        res.status(500).json({error: 'Something went wrong creating a new accessories'});
+        res.status(500).json({error: 'Something went wrong creating a new accessory'});
     }
 })
 
+// POSTMAN "POST" ROUTE --> http://localhost:3000/nike/accessories/new
+
+// BODY TEXT FOR POSTMAN TO MAKE A NEW ACCESSORY
+// {
+//     "id": "3",
+//     "itemname": "watch",
+//     "color": "gold",
+//     "size": "big"
+// }
+
+// ******************************************************************************************
+
+// UPDATE AN EXISTING ACCESSORY
+router.put('/:id', async (req, res) => {
+    try {
+        const accessoriesId = parseInt(req.params.id);
+        const { itemname, color, size } = req.body;
+
+        const updateItem = await prisma.accessories.update({
+            where: {id: parseInt(accessoriesId)},
+            data: {
+                itemname,
+                color,
+                size
+            }
+        });
+       return  res.json(updateItem);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error: 'Something went wrong updating an accessory'});
+    }
+})
+
+// POSTMAN "PUT" ROUTE --> http://localhost:3000/nike/accessories/1
+// Change the "1" to the id number of the item you want to update
+
+// BODY TEXT FOR POSTMAN TO MAKE AN UPDATE
+// {
+//     "itemname": "necklace",
+//     "color": "silver",
+//     "size": "28 inch"
+// }
+
+// ******************************************************************************************
+
+// DELETE A ACCESSORY
+router.delete('/:id', async (req, res) => {
+    try {
+        const accessoriesId = parseInt(req.params.id);
+        await prisma.accessories.delete({
+            where: {id: accessoriesId}
+        })
+        return res.json({message: 'Accessory deleted'});
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({error: 'Something went wrong deleting an accessory'});
+    }
+})
+
+// POSTMAN "DELETE" ROUTE --> http://localhost:3000/nike/accessories/1
+// Change the "1" to the id number of the item you want to delete
 
 module.exports = router;
--- SERVER/server.js
+++ SERVER/server.js
@@ -5,10 +5,12 @@ const port = 3000;
 
 app.use(bodyParser.json());
 
-
+// Route imports for express router
 app.use('/nike/accessories', require('./routes/accessories'));
-
-
+app.use('/nike/tops', require('./routes/tops'));
+app.use('/nike/bottoms', require('./routes/bottoms'));
+app.use('/nike/shoes', require('./routes/shoes'));
+app.use('/nike/users', require('./routes/users'));
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`)
