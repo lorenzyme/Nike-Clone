13187450688b5945b364c82eeba8b72b13883e34
Shamorr Williams <shamorr32123@gmail.com>
2023-11-16 21:02:43 -0500

    feat: [Redux] - fixed the search slice by adding the import to the root.js file and adding the action type to the actionTypes file.

---
+++ client/src/app/search/search.js
@@ -0,0 +1,25 @@
+import { SEARCH_PRODUCTS } from "../actionTypes";
+
+const initialState = () => {
+    return null
+};
+
+const handleSearch = (state, action) => {
+    return action.payload
+};
+
+export const searchProducts = (payload) => {
+    return {
+        type: SEARCH_PRODUCTS,
+        payload
+    }
+};
+
+export const search = (state = initialState(), action = {}) => {
+    const actionHandlers = {
+        [SEARCH_PRODUCTS]: handleSearch
+    }
+
+    const reducer = actionHandlers[action.type];
+    return reducer ? reducer(state, action): state
+};
