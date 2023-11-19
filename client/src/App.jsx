bb057beed33f22902d192726a5b312b45a48a698
Lorenzo <lorenzogardner@Lorenzos-MacBook-Pro.local>
2023-11-19 13:42:39 -0500

    Feat : [Search Bar - Init]

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route, Link, useLocation, useNavigate } from "react-router-dom"
 import { useEffect } from "react";
 import { useSelector, useDispatch, } from "react-redux";
 import { storeUser } from "./app/users/users";
-import axios from 'axios'
+import axios from 'axios';
 import "./Styles.css";
 
 
