c1b422e1f2ef31835b85c0f4efcebb5a0ca6d8ee
Lorenzo <lorenzogardner@Lorenzos-MacBook-Pro.local>
2023-12-13 22:47:02 -0500

    added footer, fixed top bar on pages

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,6 +4,8 @@ import { useNavigate, Routes, Route, Link } from "react-router-dom";
 import { FaRegHeart } from "react-icons/fa";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { useSelector } from "react-redux";
+import { DiGithubFull } from "react-icons/di";
+import { FaGithub } from "react-icons/fa";
 
 import Mens from "./products/Mens";
 import Womens from "./products/Womens";
@@ -11,52 +13,48 @@ import Kids from "./products/Kids";
 import Cart from "./checkout/Cart";
 
 const Home = () => {
+  const { cartTotalQuantity } = useSelector((state) => state.cart);
 
-  const {cartTotalQuantity} = useSelector(state => state.cart)
-  
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     <>
       <div>
         <div id="catagories-bar">
           <Routes>
-            <Route path='/mens' element={<Mens />}/>
-            <Route path='/womens' element={<Womens />}/>
-            <Route path='/kids' element={<Kids />}/>
-            <Route path='/cart' element={<Cart />}/>
+            <Route path="/mens" element={<Mens />} />
+            <Route path="/womens" element={<Womens />} />
+            <Route path="/kids" element={<Kids />} />
+            <Route path="/cart" element={<Cart />} />
           </Routes>
-          <Link to='/mens' id='mens-button-link'>
-            <button>
-              Men
-            </button>
+          <Link to="/mens" id="mens-button-link">
+            <button className="category-btn">Men</button>
           </Link>
-          <Link to='/womens' id='womens-button-link'>
-            <button>
-              Women
-            </button>
+          <Link to="/womens" id="womens-button-link">
+            <button className="category-btn">Women</button>
           </Link>
-          <Link to='/kids' id='kids-button-link'>
-            <button>
-              Kids
-            </button>
-            </Link>
+          <Link to="/kids" id="kids-button-link">
+            <button className="category-btn">Kids</button>
+          </Link>
         </div>
-        <button id="cart-button" onClick={() => navigate('/cart')}><AiOutlineShoppingCart />{cartTotalQuantity}</button>
+        <button id="cart-button" onClick={() => navigate("/cart")}>
+          <AiOutlineShoppingCart />
+          {cartTotalQuantity}
+        </button>
         <div className="secondary-navbar">
           <SearchBar />
-          <button id="wishlist-button" onClick={() => navigate('/wishlist')}><FaRegHeart /></button>
-
-        </div>
-        
-        <div id="scroll-container">
-          <h1 id="scrolling-text">JUST DO IT.</h1>
+          <button id="wishlist-button" onClick={() => navigate("/wishlist")}>
+            <FaRegHeart />
+          </button>
         </div>
 
-        <div id="space">
-          <h3 id="blank">.</h3><h3 className="coupon">⏰ Flash Sale | Extra 25% Off Select Styles</h3>
-          <p className="desc-text"> Sign in and use code <a id="coupon-code">FLASH</a> at checkout</p>
-          <br />
-        </div>
+        <div id="space"></div>
+        <h3 id="blank">.</h3>
+        <h3 className="coupon">⏰ Flash Sale | Extra 25% Off Select Styles</h3>
+        <br />
+        <p className="coupon-text">
+          Sign in and use code <a id="coupon-code">FLASH</a> at checkout
+        </p>
+        <br />
 
         <img
           id="home-banner"
@@ -64,7 +62,10 @@ const Home = () => {
           alt="product and models displayed with holiday backdrop"
         ></img>
 
-        <h1 id="just-do-it">JUST DO IT</h1>
+        <div id="scroll-container">
+          <h1 id="scrolling-text">JUST DO IT.</h1>
+        </div>
+
         <br />
         <br />
         <br />
@@ -72,8 +73,63 @@ const Home = () => {
       </div>
 
       <Carousel />
-
-      <div id="footer"></div>
+      <br />
+      <br />
+      <div id="footer">
+        <ul className="social">
+          <li>
+            <a id="gh-button" href="https://github.com/lorenzyme/Nike-Clone">
+              <FaGithub />
+              <DiGithubFull />
+            </a>
+          </li>
+        </ul>
+        <ul>
+          <li class="footer-item">
+            <a className="footer-links" href="#">
+              Home
+            </a>
+          </li>
+          <li class="footer-item">
+            <a className="footer-links" href="#">
+              Support
+            </a>
+          </li>
+          <li class="footer-item">
+            <a className="footer-links" href="#">
+              Team
+            </a>
+          </li>
+          <li class="footer-item">
+            <a className="footer-links">Contact</a>
+          </li>
+        </ul>
+        <div id="label-f">
+        <h2 id="label-f">FIND A NEARBY STORE</h2>
+        <br />
+        <h2 id="label-f">BECOME A MEMBER</h2>
+        <br />
+        <h2 id="label-f">ALREADY A MEMBER</h2>
+        <br />
+        <h2 id="label-f">SIGNUP FOR EMAIL</h2>
+        <br />
+        <h2 id="label-f">SEND US FEEDBACK</h2>
+        <br />
+        <h2 id="label-f">GET HELP</h2>
+        <br />
+        </div> 
+        <br />
+        <div id="light-f">
+          <h3>Order Status</h3>
+          <br />
+          <h3>Returns</h3>
+          <br />
+          <h3>Gifts</h3>
+          <br />
+          <h3>Careers</h3>
+          <br />
+        </div>
+      </div>
     </>
   );
 };
